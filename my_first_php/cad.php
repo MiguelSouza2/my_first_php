d80f2369af345a620598e2f24e37c1e6cae630d3
Miguel Isack verdadeiro <130250696+MiguelSouza2@users.noreply.github.com>
2024-03-14 15:26:37 -0300

    Add files via upload

---
+++ my_first_php/cad.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <main>
+        <?php 
+            var_dump($_GET);
+        ?>
+    </main>
+</html>
